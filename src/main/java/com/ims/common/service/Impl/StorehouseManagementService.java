f0b825a3659bf7fcb734f0268afd6068753a8c3d
lion <njy1822574439@outlook.com>
2022-04-05 13:24:03 +0800

    first

---
+++ src/main/java/com/ims/common/service/Impl/StorehouseManagementService.java
@@ -0,0 +1,129 @@
+package com.ims.common.service.Impl;
+
+import com.github.pagehelper.PageHelper;
+import com.ims.common.service.Interface.StorehouseManagement;
+import com.ims.common.util.Response;
+import com.ims.dao.StorehouseMapper;
+import com.ims.domain.Storehouse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StorehouseManagementService implements StorehouseManagement {
+
+    @Autowired
+    StorehouseMapper storehouseMapper;
+
+    @Override
+    public String addStorehouse(Storehouse storehouse) {
+        Response response = Response.generateResponse();
+        if(storehouse != null){
+            try {
+                storehouseMapper.addStorehouse(storehouse);
+                response.success();
+            }catch (Exception e){
+                e.printStackTrace();
+                response.exception("添加失败");
+            }
+        }
+        else{
+            response.exception("参数不足");
+        }
+        return response.toJSONString();
+    }
+
+    @Override
+    public String selectStorehouseById(Integer storehouseId) {
+        List<Storehouse> storehouses = storehouseMapper.selectStorehouseById(storehouseId);
+        return selectCommon(storehouses);
+    }
+
+    @Override
+    public String selectStorehouseByRegion(String region, Integer offset, Integer limit) {
+        if(offset >= 0 && limit > 0)
+            PageHelper.offsetPage(offset, limit);
+        List<Storehouse> storehouses = storehouseMapper.selectStorehouseByRegion(region);
+        return selectCommon(storehouses);
+    }
+
+    @Override
+    public String selectStorehouseEnabled(Integer offset, Integer limit) {
+        if(offset >= 0 && limit > 0)
+            PageHelper.offsetPage(offset, limit);
+        List<Storehouse> storehouses = storehouseMapper.selectStorehouseEnabled();
+        return selectCommon(storehouses);
+    }
+
+    @Override
+    public String selectStorehouseDisabled(Integer offset, Integer limit) {
+        if(offset >= 0 && limit > 0)
+            PageHelper.offsetPage(offset, limit);
+        List<Storehouse> storehouses = storehouseMapper.selectStorehouseDisabled();
+        return selectCommon(storehouses);
+    }
+
+    @Override
+    public String selectAllStorehouse(Integer offset, Integer limit) {
+        if(offset >= 0 && limit > 0)
+            PageHelper.offsetPage(offset, limit);
+        List<Storehouse> storehouses = storehouseMapper.selectAllStorehouse();
+        return selectCommon(storehouses);
+    }
+
+    public String selectCommon(List<Storehouse> storehouses){
+        Response response = Response.generateResponse();
+        int length = storehouses.size();
+        if(length > 0){
+            ArrayList<String> data = new ArrayList<>();
+            for (Storehouse storehouse: storehouses){
+                data.add(storehouse.toString());
+            }
+            response.setData(data.toString());
+            response.success();
+            response.setLength(length);
+        }
+        else{
+            response.exception("找不到该仓库");
+        }
+        return response.toJSONString();
+    }
+
+    @Override
+    public String modifyStorehouse(Storehouse storehouse) {
+        Response response = Response.generateResponse();
+        if(storehouse != null){
+            try {
+                storehouseMapper.modifyStorehouse(storehouse);
+                response.success();
+            }catch (Exception e){
+                e.printStackTrace();
+                response.exception("修改出错");
+            }
+        }
+        else{
+            response.exception("参数不足");
+        }
+        return response.toJSONString();
+    }
+
+    @Override
+    public String deleteStorehouse(Storehouse storehouse) {
+        Response response = Response.generateResponse();
+        if(storehouse != null){
+            try{
+                storehouseMapper.deleteStorehouse(storehouse);
+                response.success();
+            }catch (Exception e){
+                e.printStackTrace();
+                response.exception("删除出错");
+            }
+        }
+        else {
+            response.exception("参数不足");
+        }
+        return response.toJSONString();
+    }
+}
--- src/main/java/com/ism/security/service/Impl/AccountServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ism.security.service.Impl;
-
-import com.ism.dao.UserMapper;
-import com.ism.security.service.Interface.AccountService;
-
-public class AccountServiceImpl implements AccountService {
-    private UserMapper userMapper;
-
-}
